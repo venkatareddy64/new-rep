c7a0e6499f2ce279edf35eea398a2507f13240f1
venkatareddy <venkatareddy.singatala@gmail.com>
2020-06-16 17:29:18 +0530

    like

--- SeleniumProject/src/com/mq/DemoTest.java
+++ SeleniumProject/src/com/mq/DemoTest.java
@@ -4,10 +4,39 @@ import org.testng.annotations.Test;
 
 public class DemoTest {
   @Test
-  public void f()
+  public void g4()
   {
 	  System.out.println("demo1");
 	  System.out.println("demo2");
 	  System.out.println("demo3");
   }
+  @Test
+  public void f9()
+  {
+	  System.out.println("demo1");
+	  System.out.println("demo2");
+	  System.out.println("demo3");
+  }
+  @Test
+  public void f6()
+  {
+	  System.out.println("demo1");
+	  System.out.println("demo2");
+	  System.out.println("demo3");
+  }
+  @Test
+  public void f5()
+  {
+	  System.out.println("demo1");
+	  System.out.println("demo2");
+	  System.out.println("demo3");
+  }
+  @Test
+  public void f3()
+  {
+	  System.out.println("demo1");
+	  System.out.println("demo2");
+	  System.out.println("demo3");
+  }
+
 }
